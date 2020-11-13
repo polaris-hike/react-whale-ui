ac4b4bb4a7439ca08a70996e4b6fb3c74630f055
邬绪威 <wxw@24e.co>
2020-11-13 14:20:19 +0800

    tsconfig tslint 打包

---
+++ lib/index.tsx
@@ -0,0 +1,7 @@
+// @ts-ignore
+import add from "./test.ts";
+
+const a = 1;
+const b = 2;
+const c = add(a, b);
+console.log(c);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+
+module.exports = {
+    mode:'production',
+    entry: {// 入口
+        index: './lib/index.tsx'
+    },
+    output: {// 出口
+        path: path.resolve(__dirname, 'dist/lib'), // 操作系统目录不同,windows,linux 是 / ，mac 是\。所以得用path.resolve
+        library: 'whale',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/, // 对tsx或ts文件用awesome-typescript-loader 进行转义，因为浏览器不识别ts文件
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+};
