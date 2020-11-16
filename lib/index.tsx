2afdc112f546aeeec1ae4a064e142fa5652ff6bb
wuxuweilalala <2509165479@qq.com>
2020-11-16 21:56:39 +0800

    hooks 原理

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,37 @@
-import * as React from 'react';
-import * as  ReactDOM from 'react-dom';
-import Button from "./Button";
+import React,{useState} from 'react';
+import   ReactDOM from 'react-dom';
+//import Button from "./Button";
+const _state:any = [];
+let index = 0;
+const myUseState = (initValue:any)=>{
+    const currentIndex = index;
+    _state[currentIndex] = _state[currentIndex] === undefined ? initValue : _state[currentIndex];
 
-console.log('React');
-console.log(React);
-console.log('ReactDom');
-console.log(ReactDOM);
+    const setState = (newValue:any)=>{
+        _state[currentIndex] = newValue;
+        render()
+    }
+        index += 1
+    return [_state[currentIndex],setState]
+}
+const render =()=>{
+    index = 0;
+    ReactDOM.render(<App />,document.getElementById('root'))
+}
 
-ReactDOM.render(<Button />,document.getElementById('root'))
+const App = ()=>{
+    const [n,setN] = myUseState(0)
+    const [m,setM] = myUseState(0)
+    return (
+        <div>
+            n:{n}
+            m:{m}
+            <button onClick={()=>setN(n+1)}>n+1</button>
+            <button onClick={()=>setM(m+1)}>m+1</button>
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<App />,document.getElementById('root'))
