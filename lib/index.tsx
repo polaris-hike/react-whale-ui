22c5eeded2e65fa3185622e18ab128564ccb940d
wuxuweilalala <2509165479@qq.com>
2020-11-23 21:15:01 +0800

    npm publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,20 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Icon from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-    console.log(e);
-    console.log(111);
-};
 
-function App() {
-
-    return (
-        <div>
-            <Icon name="wechat" className='qqq' onClick={fn}/>
-            <Icon name="alipay" onClick={fn}/>
-        </div>
-    ) ;
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+export {Icon}
