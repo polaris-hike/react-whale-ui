53c57b0760b5b129438a2f12285267abf50d3077
wuxuweilalala <2509165479@qq.com>
2020-11-29 22:02:53 +0800

    dialog css scopedClass function

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -6,7 +6,9 @@ const [isVisible, setIsVisible] = useState(false);
 return (
     <div>
         <button onClick={() => setIsVisible(!isVisible)}>显示</button>
-        <Dialog visible={isVisible}/>
+        <Dialog visible={isVisible}>
+            <strong>hello world</strong>
+        </Dialog>
     </div>
 );
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import './dialog.scss';
+import Icon from '../icon/icon';
+import {scopedClassMaker} from '../classes';
 
 interface Props {
-    visible: boolean
+    visible: boolean;
 }
 
+
+
+const scopedClass = scopedClassMaker('whale-dialog');
+
 const Dialog: React.FunctionComponent<Props> = (props) => {
-    console.log(props);
     const {visible} = props;
     return (
-        visible ? <div>dialog</div> : null
+        visible ?
+            <>
+                <div className={scopedClass('mask')}>
+                </div>
+                <div className={scopedClass()}>
+                    <div className={scopedClass('close')}><Icon name="close"/></div>
+                    <header className={scopedClass('header')}>提示</header>
+                    <main className={scopedClass('main')}>{props.children}</main>
+                    <footer className={scopedClass('footer')}>
+                        <button>确认</button>
+                        <button>取消</button>
+                    </footer>
+                </div>
+            </>
+            : null
     );
 };
 
