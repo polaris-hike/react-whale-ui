a35a80de73cf510d9bf992df7b1b03ff0400c496
wuxuweilalala <2509165479@qq.com>
2020-12-09 21:43:27 +0800

    dialog eventHandle

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -6,7 +6,16 @@ const [isVisible, setIsVisible] = useState(false);
 return (
     <div>
         <button onClick={() => setIsVisible(!isVisible)}>显示</button>
-        <Dialog visible={isVisible}>
+        <Dialog visible={isVisible}
+            buttons={
+                [
+                    <button onClick={()=>setIsVisible(false)}>确认</button>,
+                    <button onClick={()=>setIsVisible(false)}>取消</button>
+                ]
+            }
+                onClose={()=>setIsVisible(false)}
+                closeOnBody={false}
+        >
             <strong>hello world</strong>
         </Dialog>
     </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {ReactElement, ReactEventHandler} from 'react';
 import './dialog.scss';
 import Icon from '../icon/icon';
 import {scopedClassMaker} from '../classes';
 import '../index.scss'
 interface Props {
     visible: boolean;
+    buttons:ReactElement[];
+    onClose:ReactEventHandler;
+    closeOnBody?:boolean
 }
 
 
@@ -12,24 +15,40 @@ interface Props {
 const scopedClass = scopedClassMaker('whale-dialog');
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
-    const {visible} = props;
+    const {visible,onClose,closeOnBody} = props;
+    const onClickClose:React.ReactEventHandler = (e)=>{
+        onClose(e)
+    }
+    const onClickMask:React.ReactEventHandler = (e)=>{
+        if(closeOnBody){
+            onClickClose(e)
+        }
+    }
     return (
         visible ?
             <>
-                <div className={scopedClass('mask')}>
+                <div className={scopedClass('mask')} onClick={onClickMask}>
                 </div>
                 <div className={scopedClass()}>
-                    <div className={scopedClass('close')}><Icon name="close"/></div>
+                    <div className={scopedClass('close')} onClick={onClickClose}><Icon name="close"/></div>
                     <header className={scopedClass('header')}>提示</header>
                     <main className={scopedClass('main')}>{props.children}</main>
                     <footer className={scopedClass('footer')}>
-                        <button>确认</button>
-                        <button>取消</button>
+                        {
+                          props.buttons.map(((button,index)=>
+                          React.cloneElement(button,{key:index})
+                          ))
+                        }
+                       {/* <button>确认</button>
+                        <button>取消</button>*/}
                     </footer>
                 </div>
             </>
             : null
     );
 };
+Dialog.defaultProps = {
+    closeOnBody:true
+}
 
 export default Dialog;
